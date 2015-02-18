67be3538c069bd86af998117ea89505bf1910c93
Jonathan Mariano <jamarian@ucsd.edu>
2015-02-18 01:03:54 -0800

    Added Table & Styling

--- routes/added.js
+++ routes/added.js
@@ -20,7 +20,7 @@ exports.add = function(req, res) {
 	console.log("New task added:");
 
 	console.log(newTask);
-	data["tasks"].push(newTask);
+	data["emotions"].push(newTask);
 	res.render('added', data);
 
 	  	
