38be146571b0d137d55a1bcce9015406751595bd
Edgardo Castro <castro.edgardo@yahoo.com>
2015-02-19 20:01:00 -0800

    Got images to go to description page
    also cleaned stuff up

--- routes/description.js
+++ routes/description.js
@@ -2,7 +2,8 @@ var data = require('../data.json');
 
 exports.addDescription = function(req, res){
   
-	
+	var emotion = req.query.emotion;
+  console.log(emotion);
 	res.render('description');
 
 
