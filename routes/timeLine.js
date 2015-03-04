a6f163a7fc7e25afcf89e2876ec021a9d27a9bda
Edgardo Castro <castro.edgardo@yahoo.com>
2015-03-04 14:50:30 -0800

    fixed double entry bug

--- routes/timeLine.js
+++ routes/timeLine.js
@@ -4,33 +4,6 @@ var data = require('../data.json');
 
 exports.addTimeLine = function(req, res) { 
 
-
-
-	if(!req.query.time){
-		res.render('timeLine', data);
-	}
-	else{
-	// Your code goes here
-	  var current = req.query.time.split("z");
-	  var timestamp = current[0]+" "+current[1]+" "+current[2]+" "+current[3]+" at "+current[4];
-	  var newEntry = 
-		{
-			'name': req.query.face,
-			'imageURL': req.query.image_url,
-			'description': req.query.description,
-			'date' : timestamp,
-		}
-		;
-	
-		console.log("New Entry has been added:");
-	
-		console.log(newEntry);
-		
-		data["emotions"].push(newEntry);
-		
-		res.render('timeLine', data);
-	}
-
 	// Your code goes here
 if(req.session.template){
     //Here instead explicitly render the index or index_alternate, we will render 
